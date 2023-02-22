9e5eb92ae51d38386d022254c85519d416b5982d
AbdelrahmanIbrahem <93586120+AbdelrahmanIbrahem@users.noreply.github.com>
2023-02-23 00:00:16 +0200

    Add files via upload

---
+++ server-side/server.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const dbConfig = require("./app/config/db.config");
+
+const app = express();
+
+
+global.__basedir = __dirname; 
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+const initRoutes = require("./app/routes");  
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+
+
+const db = require("./app/models");
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Movies application." });
+});
+
+
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+
+
+
+
+initRoutes(app); //mk 
+require("./app/routes/movie.routes")(app);
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+
+
